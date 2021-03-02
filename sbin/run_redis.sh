20c20f8d4299035a80db2ba7b2ce1b67017d7283
Midhul <midhul.v@gmail.com>
2021-03-02 06:49:14 -0500

    sciprts

--- sbin/run_redis.sh
+++ sbin/run_redis.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Usage: sbin/run_kv.sh <hosts file> ..... 
+# Usage: sbin/run_redis.sh <hosts file> ..... 
 
 sbin="`dirname "$0"`"
 sbin="`cd "$sbin"; pwd`"
@@ -14,5 +14,5 @@ kv_cores=($(cat $hosts_csv | awk '$2 == "KV" { print $5 }'))
 kv_ports=($(cat $hosts_csv | awk '$2 == "KV" { print $4 }'))
 
 for i in "${!kv_hosts[@]}"; do 
-  SERVERLIST="${kv_hosts[$i]}" $sbin/hosts.sh /local/deploy/start_kv.sh ${kv_cores[$i]} ${kv_instances[$i]} ${kv_hosts[$i]} ${kv_ports[$i]} "$@"
+  SERVERLIST="${kv_hosts[$i]}" $sbin/hosts.sh /local/deploy/start_redis.sh ${kv_cores[$i]} ${kv_instances[$i]} ${kv_hosts[$i]} ${kv_ports[$i]} "$@"
 done
