475aed89e32a9c8a5686c0b3dc885bbb2f171d75
Midhul <midhul.v@gmail.com>
2021-04-19 05:48:49 -0400

    script update

--- sbin/run_l1.sh
+++ sbin/run_l1.sh
@@ -10,7 +10,7 @@ shift;
 
 l1_hosts=($(cat $hosts_csv | awk '$2 == "L1" { print $3 }'))
 l1_instances=($(cat $hosts_csv | awk '$2 == "L1" { print $1 }'))
-l1_cores=($(cat $hosts_csv | awk '$2 == "L1" { print $5 }'))
+l1_cores=($(cat $hosts_csv | awk '$2 == "L1" { print $8 }'))
 
 for i in "${!l1_hosts[@]}"; do 
   SERVERLIST="${l1_hosts[$i]}" $sbin/hosts.sh /local/deploy/start_l1.sh ${l1_cores[$i]} ${l1_instances[$i]} "$@"
