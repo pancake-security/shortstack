2de53f2a31b61726af2fc9f27dcd29e6dbf1691a
Midhul <midhul.v@gmail.com>
2021-04-22 00:09:33 -0400

    more script updates

--- sbin/run_l2.sh
+++ sbin/run_l2.sh
@@ -10,7 +10,7 @@ shift;
 rep_factor=$1;
 shift;
 
-for ((replica=0; replica<$rep_factor; replica++)); do
+for ((replica=$rep_factor-1; replica>=0; replica--)); do
   l2_hosts=($(cat $hosts_csv | awk -v r=$replica '($2 == "L2" && $5 == r) { print $3 }'))
   l2_instances=($(cat $hosts_csv | awk -v r=$replica '($2 == "L2" && $5 == r) { print $1 }'))
   l2_cores=($(cat $hosts_csv | awk -v r=$replica '($2 == "L2" && $5 == r) { print $8 }'))
