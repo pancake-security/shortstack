4cd0b3a9e25911ad860f910b6dc1782c83819bbc
Midhul <midhul.v@gmail.com>
2021-04-28 14:34:19 -0400

    init speed up

--- init_exp.sh
+++ init_exp.sh
@@ -10,8 +10,9 @@ nredis=$5
 trace=$6
 objsz=$7
 nopushbins=$8
+noredis=$9
 
-if [[ "$pushbins" != "nopushbins" ]]
+if [[ "$nopushbins" != "nopushbins" ]]
 then
     echo "Pushing bins"
 
@@ -22,20 +23,24 @@ echo "Gen + sync hosts file"
 python3 gen_hosts_file.py zedro.hosts /local/deploy/hosts.csv $nl1 1 16 $nl2 1 16 $nl3 16 1 $nredis 1 1 1 $rep $rep
 sbin/sync.sh /local/deploy/hosts.csv
 
-echo "Starting redis"
-sbin/hosts.sh /local/deploy/stop_redis.sh; sbin/run_redis.sh /local/deploy/hosts.csv
+if [[ "$noredis" != "noredis" ]]
+then
 
-echo "Init KV store + push distinfo"
-/local/deploy/proxy_server init -h /local/deploy/hosts.csv -o $objsz -t /local/deploy/$trace -d /local/deploy/distinfo.bin && sbin/sync.sh /local/deploy/distinfo.bin
+    echo "Starting redis"
+    sbin/hosts.sh /local/deploy/stop_redis.sh; sbin/run_redis.sh /local/deploy/hosts.csv
+
+    echo "Init KV store + push distinfo"
+    /local/deploy/proxy_server init -h /local/deploy/hosts.csv -o $objsz -t /local/deploy/$trace -d /local/deploy/distinfo.bin && sbin/sync.sh /local/deploy/distinfo.bin
+fi
 
 echo "Starting proxies"
 sbin/hosts.sh /local/deploy/stop_proxys.sh; 
 sleep 2; 
 sbin/run_l3.sh /local/deploy/hosts.csv -s 24 -c 1 -y 30; 
-sleep 4; 
+sleep 10; 
 sbin/run_l2.sh /local/deploy/hosts.csv $rep -c 1 -y 30; 
-sleep 2; 
+sleep 10; 
 sbin/run_l1.sh /local/deploy/hosts.csv $rep -c 1; 
-sleep 4; 
+sleep 10; 
 /local/deploy/proxy_server manager -h /local/deploy/hosts.csv -s
 
