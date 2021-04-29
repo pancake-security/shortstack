fe3492fb2110e3e8d9ae98993076454f615363fd
Midhul <midhul.v@gmail.com>
2021-04-29 06:45:27 -0400

    stop on l3 no data read exception

--- libclient/src/shortstack_client.cpp
+++ libclient/src/shortstack_client.cpp
@@ -154,6 +154,9 @@ void shortstack_client::response_thread(int idx) {
             resp.sequence_num = readers_[idx].recv_response(_return);
         } catch(apache::thrift::transport::TTransportException e){
             std::cerr << e.what() << std::endl;
+            if(e.what() == "No more data to read.") {
+                break;
+            }
             continue;
         }
 
