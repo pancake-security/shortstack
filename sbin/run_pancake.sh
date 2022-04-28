2379a2329ac22e9e2b2a6291b52b93600c54b101
Midhul <midhul.v@gmail.com>
2022-04-28 17:07:17 -0400

    minor updates

---
+++ sbin/run_pancake.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Usage: sbin/run_l1.sh <hosts file> ..... 
+
+sbin="`dirname "$0"`"
+sbin="`cd "$sbin"; pwd`"
+
+hosts_csv=$1
+shift;
+rep_factor=1;
+shift;
+
+for ((replica=$rep_factor-1; replica>=0; replica--)); do
+  l1_hosts=($(cat $hosts_csv | awk -v r=$replica '($2 == "L1" && $5 == r) { print $3 }'))
+  l1_instances=($(cat $hosts_csv | awk -v r=$replica '($2 == "L1" && $5 == r) { print $1 }'))
+  l1_cores=($(cat $hosts_csv | awk -v r=$replica '($2 == "L1" && $5 == r) { print $8 }'))
+
+  for i in "${!l1_hosts[@]}"; do 
+    SERVERLIST="${l1_hosts[$i]}" $sbin/hosts.sh /local/deploy/start_pancake.sh ${l1_cores[$i]} ${l1_instances[$i]} "$@"
+  done
+
+  sleep 5;
+done;
+
---
+++ sbin/start_pancake.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+sbin="`dirname "$0"`"
+sbin="`cd "$sbin"; pwd`"
+
+coremask=$1
+iname=$2
+shift;
+shift;
+
+
+ulimit -n 10240
+
+sudo /local/deploy/wondershaper/wondershaper -a ens5 -c
+
+taskset -c $coremask /local/deploy/proxy_server pancake -h /local/deploy/hosts.csv -d /local/deploy/distinfo.bin -i $iname "$@" 2>/local/deploy/$iname.err 1>/local/deploy/$iname.out &
+echo "Started Pancake proxy"
